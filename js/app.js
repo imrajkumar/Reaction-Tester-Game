fd52787a5c3a42e6f1c8688b8caa5d2a57df1b72
Raj kumar yadav <rajkumar.raj.yadav.2015@gmail.com>
2018-03-29 22:55:56 +0530

    Add files via commit

---
+++ js/app.js
@@ -0,0 +1,37 @@
+ var start = new Date().getTime();
+ function getRandomColor() { 
+	   var letters = '0123456789ABCDEF'.split(''); 
+       var color = '#';
+		for (var i = 0; i < 6; i++ ) 
+	          {color += letters[Math.floor(Math.random() * 16)];} 
+          return color;
+      }
+
+function makeshapeappear(){
+	 start = new Date().getTime();
+	var top= Math.random() *300;
+	var left= Math.random() *500;
+	var width= (Math.random() *200) +50;
+	if(Math.random()>0.5){
+		document.getElementById("shape").style.borderRadius="50%";
+	}else{
+		document.getElementById("shape").style.borderRadius="0"
+	}
+     document.getElementById("shape").style.backgroundColor= getRandomColor();
+	 document.getElementById("shape").style.top= top +"px";
+	 document.getElementById("shape").style.left= left + "px";
+     document.getElementById("shape").style.width= width + "px";
+     document.getElementById("shape").style.height= width + "px";
+	document.getElementById("shape").style.display="block";
+}
+
+setTimeout(makeshapeappear, Math.random() * 3000);
+
+document.getElementById("shape").onclick = function() {
+	document.getElementById("shape").style.display="none";
+	 var end = new Date().getTime();
+	var timeTaken=(end-start)/1000;
+	document.getElementById("time").innerHTML= timeTaken +"s";
+	setTimeout(makeshapeappear, Math.random() * 3000);
+
+}
